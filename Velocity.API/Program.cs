f29706446b120d9ba18a0e4f02705470f14e95cd
Steven Garberg <steven.garberg.jr@gmail.com>
2022-05-08 15:05:10 -0400

    Added solution and projects

---
+++ Velocity.API/Program.cs
@@ -0,0 +1,25 @@
+using Velocity.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
